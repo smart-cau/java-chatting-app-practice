5bb05ff17e8315274bd4a03e980860b14a51ff95
HyeonWooJung <1995hyunwoo@gmail.com>
2021-06-07 12:32:24 +0900

    SimpleChatClientA 만듦

--- src/SimpleChat/SimpleChatClientA.java
+++ src/SimpleChat/SimpleChatClientA.java
@@ -3,9 +3,8 @@ package SimpleChat;
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
-
-
 
 public class SimpleChatClientA {
     JTextField outgoing;
@@ -14,19 +13,52 @@ public class SimpleChatClientA {
 
     public void go(){
         // GUI를 만들고 send button에 대한 리스너 등록
+        JFrame frame = new JFrame("이상할 정도로 간단한 채팅 클라이언트");
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("보내기");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
         // setUpNetworking() 메소드를 호출
-    }
-
-    private void setUpNetworking(){
-        // Socket을 만들고 PrintWriter를 만듦
-        // 그 PrintWriter를 writer 인스턴스 변수에 대입
+        setUpNetworking();
     }
 
     public class SendButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            // 텍스트 필드로부터 텍스트를 알아낸 다음
-            // writer(PrintWriter 객체)를 써서 서버로 보냄
+            try {
+                // 텍스트 필드로부터 텍스트를 알아낸 다음
+                writer.println(outgoing.getText());
+                // writer(PrintWriter 객체)를 써서 서버로 보냄
+                writer.flush();
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
         }
-    } // SentButtonListener 내부 클래스 끝
+    }
+
+    // Socket과 PrintWriter를 만드는 함수
+    private void setUpNetworking(){
+        try {
+            // Socket을 만듦
+            sock = new Socket("127.0.0.1", 5000);
+            //  PrintWriter를 만듦고, 그 PrintWriter를 writer 인스턴스 변수에 대입
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("Networking ESTABLISHED");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        new SimpleChatClientA().go();
+    }
 } // 외부 클래스 끝
